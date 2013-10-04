c9c50d68ff4aedcd748116294a7beeaa5bc39167
Zhenfang Wei <zhenfang.wei@ericsson.com>
2013-10-04 14:15:28 +0200

    Added some packages

---
+++ elpa/color-theme-tango-0.0.2/color-theme-tango-autoloads.el
@@ -0,0 +1,18 @@
+;;; color-theme-tango-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("color-theme-tango-pkg.el" "color-theme-tango.el")
+;;;;;;  (21070 45237 2672 456000))
+
+;;;***
+
+(provide 'color-theme-tango-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; color-theme-tango-autoloads.el ends here
