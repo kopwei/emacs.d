67b8d2c02a94351142b856f45dfca3651f7f48ac
Zhenfang Wei <kopkop@gmail.com>
2013-10-07 22:19:11 +0200

    correct the error for mac

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-(load-file "/home/ezhenwe/.emacs.d/emacs-for-python/epy-init.el")
-(add-to-list 'load-path "/home/ezhenwe/.emacs.d/emacs-for-python/") ;; tell where to load the various files
+(load-file "$HOME/.emacs.d/emacs-for-python/epy-init.el")
+(add-to-list 'load-path "$HOME/.emacs.d/emacs-for-python/") ;; tell where to load the various files
 (require 'epy-setup)      ;; It will setup other loads, it is required!
 (require 'epy-python)     ;; If you want the python facilities [optional]
 (require 'epy-completion) ;; If you want the autocompletion settings [optional]
