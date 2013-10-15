7c9bf48794287becf493020ac4a2626a47ef3f7c
Zhenfang Wei <kopkop@gmail.com>
2013-10-15 20:22:20 +0200

    add fci-mode hook

--- init.el
+++ init.el
@@ -21,9 +21,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fill-column 80)
  '(global-linum-mode t)
  '(global-undo-tree-mode t))
 
+(add-hook 'python-mode-hook 'fci-mode)
 (color-theme-solarized-light)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
